99d25bcb6d9cd762ef8da6ccbab793dc8df681bb
Istam <mamadaliyevistam@gmail.com>
2025-10-06 17:08:39 +0500

    first commit

---
+++ frontend/src/app/about/page.jsx
@@ -0,0 +1,13 @@
+import "./style.scss";
+
+
+function About() {
+    return ( 
+        <div className="about-page-wrapper">
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, inventore. Eaque mollitia eos, hic debitis necessitatibus, iure enim dolore numquam repellendus nam ut eius, blanditiis reprehenderit dignissimos! Impedit, dicta velit.</p>
+        </div>
+     );
+}
+
+export default About;
